1e7228fe62d1087d306985a9978bb92f80f78f06
CerenGKB <cerengokbulut@hotmail.com>
2023-06-24 23:29:14 +0300

    Small change in text

--- Tenzies/src/components/Die.jsx
+++ Tenzies/src/components/Die.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 function Die(value) {
     
-    const dice = [1,2,3,4,5,6,5,4,3,4]
+    const dice = [1,2,3,4,5,6,5,5,7,6]
     const listDice = dice.map(num => <div class ="numBox">{num}</div>)
 
     return (
