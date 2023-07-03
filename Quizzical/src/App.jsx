78bd47cd40f503e4fbdfdc5135dd054cb843f903
CerenGKB <cerengokbulut@hotmail.com>
2023-07-03 14:59:13 +0300

    Added start game page css

--- Quizzical/src/App.jsx
+++ Quizzical/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import React from 'react'
 import './App.css'
+import QuizPart from "./components/QuizPart"
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div>hello</div>
-    </>
+    <main>
+    <div className="title">Quizzical</div>
+    <div className="description">Quizzical is a trivia game where you try to 5 questions at a time</div>
+    <button className="startGame">Start Quiz</button>
+    </main>
+    
   )
 }
 
---
+++ Quizzical/src/components/QuizPart.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function QuizPart(props){
+    return (
+        <div>do i work</div>
+    )
+}
