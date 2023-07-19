31b59308291c44e70f7138a1a0d122fbf2d45352
CerenGKB <cerengokbulut@hotmail.com>
2023-07-19 16:44:07 +0300

    Started creating a dashboard for tailwind practice

---
+++ Dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <h1 className="text-3xl font-bold underline">Hello</h1>
+   </div>
+  )
+}
+
+export default App
