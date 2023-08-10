3055b7aab1a8b23c90f37a4813eb7f04ad2e6164
CerenGKB <cerengokbulut@hotmail.com>
2023-08-10 15:55:05 +0300

    Created fake api with miragejs, created van page with normal css for now

--- Van Life/src/App.jsx
+++ Van Life/src/App.jsx
@@ -4,30 +4,35 @@ import {BrowserRouter, Routes , Route, Link} from "react-router-dom"
 import Home from './components/Home'
 import Vans from './components/Vans'
 import About from './components/About'
+import "./server"  
+
+
 
 function App() {
  
 
   return (
     <main className='m-0 p-0 flex flex-col h-screen'>
-    <BrowserRouter>
-    <nav className='py-10  w-full flex flex-shrink-0 justify-between p-4'>
-    <div className='sm:text-3xl text-2xl font-bold'>
-    #VANLIFE
-    </div>
-    <div className='flex mr-4 gap-10 items-center text-xl'>
-    <Link to="/about">About </Link>
-    <Link to="/vans">Vans </Link>
-    </div>
-    </nav>
+      <BrowserRouter>
+        <nav className='py-10  w-full flex flex-shrink-0 justify-between p-4'>
+          <div className='sm:text-3xl text-2xl font-bold'>
+          <Link to="/">#VANLIFE </Link>
 
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-     </Routes>
-   
-    </BrowserRouter> 
-    <footer className='h-10 p-10 bg-ftColor w-full text-white flex justify-center items-center flex-shrink-0'>2022 #VANLIFE</footer>
+          </div>
+          <div className='flex mr-4 gap-10 items-center text-xl'>
+          <Link to="/about">About </Link>
+          <Link to="/vans">Vans </Link>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+        </Routes>
+
+      </BrowserRouter> 
+      <footer className='h-10 p-10 bg-ftColor w-full text-white flex justify-center items-center flex-shrink-0'>2022 #VANLIFE</footer>
     </main>
   )
 }
--- Van Life/src/components/Home.jsx
+++ Van Life/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from "react-router-dom"
 
 const Home = () => {
   
@@ -7,7 +7,7 @@ const Home = () => {
           <div className='bg-tent w-full h-[80vh] bg-cover bg-no-repeat flex flex-col flex-grow overflow-auto items-center justify-center text-white'>
           <p className='w-4/5  md:w-1/2 text-4xl sm:text-5xl font-extrabold py-5 '>You got the travel plans,<br/> we got the travel vans.</p>
           <p className='text-xl w-4/5 md:w-1/2 py-5'>Add adventure to your life by joining the #vanlife movement.Rent the perfect van to make your perfect road trip.</p>
-          <button className='bg-btnColor w-4/5 py-4 md:w-1/2 rounded-sm' >Find your van</button>
+          <Link className='bg-btnColor w-4/5 py-4 md:w-1/2 rounded-sm text-center' to="/vans" >Find your van</Link>
           </div>
         )
 }
