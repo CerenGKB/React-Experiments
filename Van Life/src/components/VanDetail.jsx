3338388b2dceab54a626194de5e4c5e5baa61bb0
CerenGKB <cerengokbulut@hotmail.com>
2023-08-30 20:26:50 +0300

    finished setting up van detail page

--- Van Life/src/components/VanDetail.jsx
+++ Van Life/src/components/VanDetail.jsx
@@ -1,10 +1,31 @@
 import React from "react"
-
+import {useParams} from "react-router-dom"
 
 const VanDetail = () => {
+
+    const params = useParams()
+    const [van, setVan] = React.useState(null)
+
+    React.useEffect(()=> {
+        fetch(`/api/vans/${params.id}`)
+        .then(res => res.json())
+        .then(data =>setVan(data.vans))
+    }, [params.id])
+
+
     return(
-        // this line will have details from miragejs data
-        <p></p>
+        <div className="">
+        {van ? (
+            <div className="">
+                <img src={van.imageUrl} />
+                <i className={` ${van.type}`}>{van.type}</i>
+                <h2>{van.name}</h2>
+                <p className=""><span>${van.price}</span>/day</p>
+                <p>{van.description}</p>
+                <button className="">Rent this van</button>
+            </div>
+        ) : <h2>Loading...</h2>}
+    </div>
     )
 }
 
